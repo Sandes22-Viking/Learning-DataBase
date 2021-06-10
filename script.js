0de22187958eefdae43f2cbdcf5c4ac88b758506
Erick Sandes <79476117+Sandes22-Viking@users.noreply.github.com>
2021-06-09 23:40:31 -0300

    New Update

---
+++ script.js
@@ -0,0 +1,204 @@
+function execute() {
+    let main = document.getElementById("main")
+
+    if (main.style.display === "none") {
+        main.style.display = "block"
+    } else {
+        main.style.display = "none"
+    }
+}
+
+function execute_two() {
+    let main_two = document.getElementById("main_two")
+
+    if (main_two.style.display === "none") {
+        main_two.style.display = "block"
+    } else {
+        main_two.style.display = "none"
+    }
+}
+
+function execute_three() {
+    let main_three = document.getElementById("main_three")
+
+    if (main_three.style.display === "none") {
+        main_three.style.display = "block"
+    } else {
+        main_three.style.display = "none"
+    }
+}
+
+function execute_four() {
+    let main_four = document.getElementById("main_four")
+
+    if (main_four.style.display === "none") {
+        main_four.style.display = "block"
+    } else {
+        main_four.style.display = "none"
+    }
+}
+
+function execute_five() {
+    let main_five = document.getElementById("main_five")
+
+    if (main_five.style.display === "none") {
+        main_five.style.display = "block"
+    } else {
+        main_five.style.display = "none"
+    }
+}
+
+function execute_six() {
+    let main_six = document.getElementById("main_six")
+
+    if (main_six.style.display === "none") {
+        main_six.style.display = "block"
+    } else {
+        main_six.style.display = "none"
+    }
+}
+
+function execute_seven() {
+    let main_seven = document.getElementById("main_seven")
+
+    if (main_seven.style.display === "none") {
+        main_seven.style.display = "block"
+    } else {
+        main_seven.style.display = "none"
+    }
+}
+
+function execute_eight() {
+    let main_eight = document.getElementById("main_eight")
+
+    if (main_eight.style.display === "none") {
+        main_eight.style.display = "block"
+    } else {
+        main_eight.style.display = "none"
+    }
+}
+
+function execute_nine() {
+    let main_nine = document.getElementById("main_nine")
+
+    if (main_nine.style.display === "none") {
+        main_nine.style.display = "block"
+    } else {
+        main_nine.style.display = "none"
+    }
+}
+
+function execute_ten() {
+    let main_ten = document.getElementById("main_ten")
+
+    if (main_ten.style.display === "none") {
+        main_ten.style.display = "block"
+    } else {
+        main_ten.style.display = "none"
+    }
+}
+
+function execute_eleven() {
+    let main_eleven = document.getElementById("main_eleven")
+
+    if (main_eleven.style.display === "none") {
+        main_eleven.style.display = "block"
+    } else {
+        main_eleven.style.display = "none"
+    }
+}
+
+function execute_twelve() {
+    let main_twelve = document.getElementById("main_twelve")
+
+    if (main_twelve.style.display === "none") {
+        main_twelve.style.display = "block"
+    } else {
+        main_twelve.style.display = "none"
+    }
+}
+
+function execute_thirteen() {
+    let main_thirteen = document.getElementById("main_thirteen")
+
+    if (main_thirteen.style.display === "none") {
+        main_thirteen.style.display = "block"
+    } else {
+        main_thirteen.style.display = "none"
+    }
+}
+
+function execute_fourteen() {
+    let main_fourteen = document.getElementById("main_fourteen")
+
+    if (main_fourteen.style.display === "none") {
+        main_fourteen.style.display = "block"
+    } else {
+        main_fourteen.style.display = "none"
+    }
+}
+
+function execute_fifteen() {
+    let main_fifteen = document.getElementById("main_fifteen")
+
+    if (main_fifteen.style.display === "none") {
+        main_fifteen.style.display = "block"
+    } else {
+        main_fifteen.style.display = "none"
+    }
+}
+
+function execute_sixteen() {
+    let main_sixteen = document.getElementById("main_sixteen")
+
+    if (main_sixteen.style.display === "none") {
+        main_sixteen.style.display = "block"
+    } else {
+        main_sixteen.style.display = "none"
+    }
+}
+
+function execute_seventeen() {
+    let main_seventeen = document.getElementById("main_seventeen")
+
+    if (main_seventeen.style.display === "none") {
+        main_seventeen.style.display = "block"
+    } else {
+        main_seventeen.style.display = "none"
+    }
+}
+
+function execute_eighteen() {
+    let main_eighteen = document.getElementById("main_eighteen")
+
+    if (main_eighteen.style.display === "none") {
+        main_eighteen.style.display = "block"
+    } else {
+        main_eighteen.style.display = "none"
+    }
+}
+
+function execute_nineteen() {
+    let main_nineteen = document.getElementById("main_nineteen")
+
+    if (main_nineteen.style.display === "none") {
+        main_nineteen.style.display = "block"
+    } else {
+        main_nineteen.style.display = "none"
+    }
+}
+
+function execute_twenty() {
+    let main_twenty = document.getElementById("main_twenty")
+
+    if (main_twenty.style.display === "none") {
+        main_twenty.style.display = "block"
+    } else {
+        main_twenty.style.display = "none"
+    }
+}
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
